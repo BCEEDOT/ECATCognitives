4c4c0180f501c9d102cebce3e1ab27ac0af71a7d
Jason <enderjs@gmail.com>
2017-05-08 06:24:59 -0500

    --Add file

---
+++ src/app/student/services/assessment.resolver.ts
@@ -0,0 +1,15 @@
+import { Injectable, OnInit } from '@angular/core';
+import { Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+
+@Injectable()
+export class AssessmentResolver {
+
+
+
+  constructor() {
+    
+  }
+
+}
