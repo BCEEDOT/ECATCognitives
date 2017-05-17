3f6a66aa92c22d9e0863576fd5f00f55b722d044
DESKTOP-7892F9D\ender <enderjs>
2017-05-17 08:50:31 -0500

    --Update to Angular 4.1.2, covalent beta4, material beta 5, breeze bridge 4.0

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,23 +1,57 @@
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { NgModule, Type } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from "@angular/forms";
-
-import { CovalentCoreModule } from '@covalent/core';
+import {
+    CovalentCommonModule, CovalentLayoutModule, CovalentMediaModule, CovalentExpansionPanelModule, CovalentFileModule,
+    CovalentStepsModule, CovalentLoadingModule, CovalentDialogsModule, CovalentSearchModule, CovalentPagingModule,
+    CovalentNotificationsModule, CovalentMenuModule, CovalentChipsModule, CovalentDataTableModule, CovalentJsonFormatterModule
+} from '@covalent/core';
+import {
+    MdButtonModule, MdListModule, MdIconModule, MdCardModule, MdMenuModule, MdInputModule, MdButtonToggleModule,
+    MdProgressSpinnerModule, MdSelectModule, MdSlideToggleModule, MdDialogModule, MdSnackBarModule, MdToolbarModule,
+    MdTabsModule, MdSidenavModule, MdTooltipModule, MdCheckboxModule, MdRadioModule, MdCoreModule, MdAutocompleteModule,
+    MdProgressBarModule, MdSliderModule, MdChipsModule, MdGridListModule, MdRippleModule
+} from '@angular/material';
 import { CovalentHighlightModule } from '@covalent/highlight';
 import { CovalentMarkdownModule } from '@covalent/markdown';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @NgModule({
     imports: [
-        CovalentCoreModule,    
+        FlexLayoutModule,
+        CommonModule,
+        FormsModule,
+        MdButtonModule, MdListModule, MdIconModule, MdCardModule, MdMenuModule, MdInputModule, MdButtonToggleModule,
+        MdProgressSpinnerModule, MdSelectModule, MdSlideToggleModule, MdDialogModule, MdSnackBarModule, MdToolbarModule,
+        MdTabsModule, MdSidenavModule, MdTooltipModule, MdCheckboxModule, MdRadioModule, MdCoreModule, MdAutocompleteModule,
+        MdProgressBarModule, MdSliderModule, MdChipsModule, MdGridListModule, MdRippleModule,
+        CovalentCommonModule, CovalentLayoutModule, CovalentMediaModule, CovalentExpansionPanelModule, CovalentFileModule,
+        CovalentStepsModule, CovalentLoadingModule, CovalentDialogsModule, CovalentSearchModule, CovalentPagingModule,
+        CovalentNotificationsModule, CovalentMenuModule, CovalentChipsModule, CovalentDataTableModule, CovalentJsonFormatterModule,
         CovalentHighlightModule,
         CovalentMarkdownModule,
-        NgxChartsModule,
-        CommonModule,
+        NgxChartsModule
+
     ],
     declarations: [
+
     ],
-    exports: [CovalentCoreModule, CovalentHighlightModule, CovalentMarkdownModule, FormsModule, CommonModule],
+    exports: [
+        FlexLayoutModule,
+        CommonModule,
+        FormsModule,
+        MdButtonModule, MdListModule, MdIconModule, MdCardModule, MdMenuModule, MdInputModule, MdButtonToggleModule,
+        MdProgressSpinnerModule, MdSelectModule, MdSlideToggleModule, MdDialogModule, MdSnackBarModule, MdToolbarModule,
+        MdTabsModule, MdSidenavModule, MdTooltipModule, MdCheckboxModule, MdRadioModule, MdCoreModule, MdAutocompleteModule,
+        MdProgressBarModule, MdSliderModule, MdChipsModule, MdGridListModule, MdRippleModule,
+        CovalentCommonModule, CovalentLayoutModule, CovalentMediaModule, CovalentExpansionPanelModule, CovalentFileModule,
+        CovalentStepsModule, CovalentLoadingModule, CovalentDialogsModule, CovalentSearchModule, CovalentPagingModule,
+        CovalentNotificationsModule, CovalentMenuModule, CovalentChipsModule, CovalentDataTableModule, CovalentJsonFormatterModule,
+        CovalentHighlightModule,
+        CovalentMarkdownModule,
+        NgxChartsModule
+    ],
     providers: []
 })
 
