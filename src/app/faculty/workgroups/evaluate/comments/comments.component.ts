badcf5546d5d9e42a3cb9b02c47df5084a656468
Jeremiah Beckham <jbeck371@yahoo.com>
2017-06-16 12:06:05 -0500

    -- Continuation of review comments screen

--- src/app/faculty/workgroups/evaluate/comments/comments.component.ts
+++ src/app/faculty/workgroups/evaluate/comments/comments.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Location } from '@angular/common';
+import { MdSnackBar } from '@angular/material';
+import { TdDialogService } from "@covalent/core";
 
 import { WorkGroup, CrseStudentInGroup, StudSpComment } from "../../../../core/entities/faculty";
 import { ActivatedRoute } from "@angular/router";
 import { FacultyDataContextService } from "../../../services/faculty-data-context.service";
+import { MpCommentFlag } from "../../../../core/common/mapStrings";
 
 @Component({
   selector: 'comments',
@@ -12,15 +15,18 @@ import { FacultyDataContextService } from "../../../services/faculty-data-contex
 })
 export class CommentsComponent implements OnInit {
 
-  private memsWithComments: CrseStudentInGroup[] = [];
+  private memsWithComments: CrseStudentInGroup[];
   private selectedAuthor: CrseStudentInGroup;
   private authoredComments: StudSpComment[];
   private selectedComment: StudSpComment;
   private grpHasComments: boolean = true;
+  private commFlagMap = MpCommentFlag;
 
   constructor(private route: ActivatedRoute,
     private location: Location,
-    private ctx: FacultyDataContextService) { }
+    private ctx: FacultyDataContextService,
+    private snackBar: MdSnackBar,
+    private dialogService: TdDialogService) { }
 
   @Input() members: CrseStudentInGroup[];
 
@@ -29,10 +35,12 @@ export class CommentsComponent implements OnInit {
   }
 
   activate() {
+    this.memsWithComments = [];
     this.members.forEach(mem => {
       if (mem.authorOfComments.length > 0){
-        mem['reviewIncomplete'] = mem.authorOfComments.some(com => com.flag.mpFaculty === null);
-        mem['icon'] = (mem['reviewIncomplete']) ? "indeterminate_check_box": "check_box";
+        mem['numRemaining'] = mem.authorOfComments.filter(com => com.flag.mpFaculty === null).length;
+        mem['apprFlags'] = mem.authorOfComments.filter(com => com.flag.mpFaculty === MpCommentFlag.appr).length;
+        mem['inapprFlags'] = mem.authorOfComments.filter(com => com.flag.mpFaculty === MpCommentFlag.inappr).length;
         this.memsWithComments.push(mem);
       }
     });
@@ -48,27 +56,81 @@ export class CommentsComponent implements OnInit {
   changeAuthor(sel: CrseStudentInGroup){
     this.selectedAuthor = sel;
     this.authoredComments = this.selectedAuthor.authorOfComments;
-    this.setUpComments();
-    this.selectedComment = this.authoredComments[0];
-  }
-
-  checkComplete(auth: CrseStudentInGroup){
-    auth.authorOfComments.forEach(com => {
-      if (com.flag.mpFaculty !== null){
-        auth['reviewIncomplete'] = false;
-      }
-    });
-  }
-
-  setUpComments() {
+    
     this.authoredComments.sort((a: StudSpComment, b: StudSpComment) => {
       if (a.recipient.studentProfile.person.lastName > b.recipient.studentProfile.person.lastName) {return 1;}
       if (a.recipient.studentProfile.person.lastName < b.recipient.studentProfile.person.lastName) {return -1;}
       return 0;
     });
 
-    this.authoredComments.forEach(com => {
-      com['icon'] = (com.flag.mpFaculty === null) ? "indeterminate_check_box": "check_box";
+    this.checkComplete();
+    this.selectedComment = this.authoredComments[0];
+  }
+
+  checkComplete(){
+    this.selectedAuthor.authorOfComments.forEach(com => {
+      switch(com.flag.mpFaculty){
+        case MpCommentFlag.appr: com['icon'] = 'done'; break;
+        case MpCommentFlag.inappr: com['icon'] = 'highlight_off'; break;
+        default: com['icon'] = '';
+      }
+
+      com['displayDate'] = com.modifiedDate.toDateString() + ' ' + com.modifiedDate.getHours().toString() + ':' + com.modifiedDate.getMinutes().toString();
     });
+
+    this.selectedAuthor['numRemaining'] = this.selectedAuthor.authorOfComments.filter(com => com.flag.mpFaculty === null).length;
+    this.selectedAuthor['apprFlags'] = this.selectedAuthor.authorOfComments.filter(com => com.flag.mpFaculty === MpCommentFlag.appr).length;
+    this.selectedAuthor['inapprFlags'] = this.selectedAuthor.authorOfComments.filter(com => com.flag.mpFaculty === MpCommentFlag.inappr).length;
+  }
+
+  massSetAll(){
+    let allUnflagged = this.members[0].workGroup.spComments.filter(com => com.flag.mpFaculty === null);
+    allUnflagged.forEach(com => com.flag.mpFaculty = MpCommentFlag.appr);
+    this.activate();
+  }
+
+  massResetAll(){
+    let allComments = this.members[0].workGroup.spComments;
+    allComments.forEach(com => com.flag.mpFaculty = MpCommentFlag.appr);
+    this.activate();
+  }
+
+  massSetAuthor(){
+    let authUnflagged = this.authoredComments.filter(com => com.flag.mpFaculty === null);
+    authUnflagged.forEach(com => com.flag.mpFaculty = MpCommentFlag.appr);
+    this.checkComplete();
+  }
+
+  massResetAuthor(){
+    this.authoredComments.forEach(com => com.flag.mpFaculty = MpCommentFlag.appr);
+    this.checkComplete();
+  }
+
+  save() {
+    this.ctx.commit().then(fulfilled => {
+      this.snackBar.open('Comment Flags Saved!', 'Dismiss');
+    }, (reject => {
+      this.dialogService.openAlert({message: 'There was a problem saving your flags, please try again.', title: 'Save Error'});
+    }));
+  }
+
+  discard() {
+    let changes = this.selectedAuthor.workGroup.spComments.filter(com => com.flag.entityAspect.entityState.isAddedModifiedOrDeleted());
+
+    if (changes.length > 0){
+      this.dialogService.openConfirm({
+          message: 'You have ' + changes.length + ' unsaved comment flag changes. Are you sure you want to discard them?',
+          title: 'Discard Changes',
+          acceptButton: 'Yes',
+          cancelButton: 'No'
+      }).afterClosed().subscribe((confirmed: boolean) => {
+        if (confirmed) {
+          changes.forEach(com => com.flag.entityAspect.rejectChanges());
+          this.activate();
+        }
+      });
+    } else {
+      this.dialogService.openAlert({message: 'No changes to discard.', title: 'Discard Changes'});
+    }
   }
 }
