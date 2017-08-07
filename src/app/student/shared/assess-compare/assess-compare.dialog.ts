db4885387616d8f080d7be7f11ebe51354ae2629
Jeremiah Beckham <jbeck371@yahoo.com>
2017-08-07 12:00:38 -0500

    -- Accesscompare chart using real data

--- src/app/student/shared/assess-compare/assess-compare.dialog.ts
+++ src/app/student/shared/assess-compare/assess-compare.dialog.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit, Inject, ViewChild, TemplateRef } from '@angular/core';
+import { Component, OnInit, Inject, ViewChild, TemplateRef, ContentChild } from '@angular/core';
 import { MdDialog, MdDialogRef, MdDialogConfig, MD_DIALOG_DATA } from '@angular/material';
 import { TdChipsComponent } from "@covalent/core";
 import { NgxChartsModule } from '@swimlane/ngx-charts';
-import { multi } from './data';
-import { single} from './singledata';
+import { WorkGroup, SpResponse } from "../../../core/entities/student/index";
 
 
 @Component({
@@ -15,14 +14,16 @@ import { single} from './singledata';
 export class AssessCompareDialog implements OnInit {
 
   @ViewChild(TemplateRef) template: TemplateRef<any>;
+  @ContentChild('tooltipTemplate') tooltipTemplate: TemplateRef<any>;
 
-  single: any[];
-  multi: any[];
+  single: any[] = [];
+  multi: any[] = [];
   multiOriginal: any[];
   readOnly: boolean = false;
   chipAddition: boolean = true;
   itemsRequireMatch: string[] = [];
   items: string[] = [];
+  //behaviorKey: string[] = [];
 
   view: any[] = [900, 400];
 
@@ -37,17 +38,49 @@ export class AssessCompareDialog implements OnInit {
   yAxisLabel = 'Rating';
 
   blueOrangeScheme: any = {
-    domain: ['#0D47A1', '#FFCC80', '#1976D2', '#039BE5', '#00BCD4', '#FB8C00', '#FFA726', '#FFCC80', '#FFECB3'],
+    domain: ['#FF0101', '#FF7800', '#FFF300', '#8BFF00', '#08FF00', '#00FFBD', '#00A6FF', '#0008FF', '#7000FF'],
+    //domain: ['#0D47A1', '#FFCC80', '#1976D2', '#039BE5', '#00BCD4', '#FB8C00', '#FFA726', '#FFCC80', '#FFECB3'],
   };
 
   constructor(public dialogRef: MdDialogRef<AssessCompareDialog>, private chips: TdChipsComponent,
     @Inject(MD_DIALOG_DATA) public data: any) {
 
-    Object.assign(this, {single, multi});
+    let grp: WorkGroup = data.workGroup;
+    grp.groupMembers.forEach(mem => {
+      let memData = { 
+        name: '',
+        series: []
+      };
+      memData.name = mem.studentProfile.person.firstName.slice(0,1) + '. ' + mem.studentProfile.person.lastName;
+      if (mem.assesseeSpResponses || mem.assesseeSpResponses.length > 0){
+        mem.assesseeSpResponses.sort((a: SpResponse, b: SpResponse) => {
+          if (a.inventoryItem.displayOrder < b.inventoryItem.displayOrder) {return -1;}
+          if (a.inventoryItem.displayOrder > b.inventoryItem.displayOrder) {return 1;}
+          return 0;
+        });
+
+        let i = 1;
+        mem.assesseeSpResponses.forEach(resp => {
+          let behavior = {
+            name: '',
+            value: 0,
+          };
+          behavior.name = resp.inventoryItem.behavior.slice(0, 15);
+          behavior.value = resp.itemModelScore;
+          memData.series.push(behavior);
+          // if (this.behaviorKey.length < mem.assesseeSpResponses.length) {
+          //   let invKey = 'Behavior ' + i + ': ' + resp.inventoryItem.behavior;
+          //   this.behaviorKey.push(invKey);
+          // }
+          i++;
+        });
+      }
+      this.multi.push(memData);
+    });
     this.multiOriginal = this.multi;
 
     this.multi.forEach(data => {
-      this.items.push(data.name);
+      //this.items.push(data.name);
       this.itemsRequireMatch.push(data.name);
     })
 
@@ -70,15 +103,17 @@ export class AssessCompareDialog implements OnInit {
       return match;
     });
 
+    this.items = this.items.filter(item => item !== value);
   }
 
   remove(value: string): void {
-    console.log(value);
+    //console.log(value);
+    this.items.push(value);
     this.multi = this.multi.filter(data => data.name != value);
   }
 
   ngOnInit() {
-    console.log(this.itemsRequireMatch);
+    //console.log(this.itemsRequireMatch);
   }
 
   // onSelect(event) {
