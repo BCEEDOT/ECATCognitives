7afa1cf6a5fb9ec9d341b9c7df57ed62d9fe8e76
DESKTOP-7892F9D\ender <enderjs>
2017-09-05 13:02:00 -0500

    --Match td-message style to student assesement screen. ---Linting

--- src/app/roadrunner/roadrunner-details/roadrunner-details.component.ts
+++ src/app/roadrunner/roadrunner-details/roadrunner-details.component.ts
@@ -1,17 +1,16 @@
-import { Component, OnInit, OnDestroy } from '@angular/core'
-import { RoadrunnerService } from '../services/roadrunner.service';
-import { UserDataContext } from "../../core/services/data/user-data-context.service";
-import { RoadRunner } from "../../core/entities/user";
-import { Router } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 import { MdSnackBar } from '@angular/material';
-import { Location } from '@angular/common'
-
-import { isEmpty} from "lodash"; 
-import { Subscription } from "rxjs/Subscription";
-
+import { Location } from '@angular/common';
+import { TdDialogService } from '@covalent/core';
+import { isEmpty } from 'lodash';
+import { Subscription } from 'rxjs/Subscription';
 import { ReactiveFormsModule, FormsModule, FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
-import { ActivatedRoute } from '@angular/router';
+
+import { RoadrunnerService } from '../services/roadrunner.service';
+import { UserDataContext } from '../../core/services/data/user-data-context.service';
+import { RoadRunner } from '../../core/entities/user';
 
 @Component({
     templateUrl: './roadrunner-details.component.html',
@@ -32,44 +31,39 @@ export class RoadrunnerDetailsComponent implements OnInit, OnDestroy {
     startCheck: boolean = false;
     endCheck: boolean = false;
     touch: boolean = true;
-    minDate = new Date(2017, 0, 1);
-    maxDate = new Date(2020, 0, 1);
+    minDate: Date = new Date(2017, 0, 1);
+    maxDate: Date = new Date(2020, 0, 1);
     clone: boolean = false;
-
-
     dateForm: FormGroup;
 
     constructor(private roadRunnerService: RoadrunnerService,
         private userDataContext: UserDataContext,
         private route: ActivatedRoute,
         private router: Router,
+        private dialogService: TdDialogService,
         private snackBarService: MdSnackBar,
         private location: Location,
     ) {
 
     }
 
-    ngOnInit() {
+    ngOnInit(): void {
 
         this.eventSub = this.roadRunnerService.roadRunnerData$.subscribe((roadRunnerData: RoadRunner[]) => {
-            console.log("roadrunner update")
+            //console.log("roadrunner update")
             this.event = roadRunnerData;
-        })
-
-        
-
+        });
         this.checkNew = (this.route.snapshot.params['id']);
 
-        if(isEmpty(this.event) && this.checkNew != "New"){
-            this.router.navigate(['roadrunnerStudent/']);
+        if (isEmpty(this.event) && this.checkNew !== "New") {
+            this.router.navigate(['roadrunner/student/']);
         }
-
 
         if (this.checkNew === "New") {
             this.oneEvent = this.userDataContext.addRoadRunner();
-            var today = new Date();
-            this.oneEvent.leaveDate = today;
-            this.oneEvent.returnDate = today;
+            let now: Date = new Date();
+            this.oneEvent.leaveDate = now;
+            this.oneEvent.returnDate = now;
 
         } else {
             this.id = (+this.route.snapshot.params['id']);
@@ -81,45 +75,51 @@ export class RoadrunnerDetailsComponent implements OnInit, OnDestroy {
 
                 this.oneEvent = this.userDataContext.addRoadRunner();
                 this.clone = true;
-                var today = new Date();
+                let now: Date = new Date();
                 this.oneEvent.location = this.tempEvent.location;
                 this.oneEvent.phoneNumber = this.tempEvent.phoneNumber;
-                this.oneEvent.leaveDate = today;
-                this.oneEvent.returnDate = today;
+                this.oneEvent.leaveDate = now;
+                this.oneEvent.returnDate = now;
 
             }
         }
     }
 
-    ngOnDestroy() {
+    ngOnDestroy(): void {
         this.eventSub.unsubscribe();
     }
 
-    cancel() {
+    cancel(): void {
 
         this.oneEvent.entityAspect.rejectChanges();
         this.location.back();
 
     }
 
-    save() {
+    save(): void {
         this.userDataContext.commit()
             .then((res) => {
-                console.log('check roadrunner database');
-                this.snackBarService.open('Roadrunner Data Saved', 'Dismiss', {duration: 2000});
-                this.router.navigate(['roadrunnerStudent/']);
-            })
+                this.snackBarService.open('Roadrunner Data Saved', 'Dismiss', { duration: 2000 });
+                this.router.navigate(['roadrunner/student/']);
+            }).catch((error) => {
+                this.dialogService.openAlert({
+                    message: 'There was an error saving your changes, please try again.',
+                });
+            });
     }
 
-    delete() {
+    delete(): void {
 
         this.oneEvent.entityAspect.setDeleted();
 
         this.userDataContext.commit()
             .then((res) => {
-                console.log('check roadrunner database');
-                this.router.navigate(['roadrunnerStudent/']);
-            })
+                this.router.navigate(['roadrunner/student/']);
+            }).catch((error) => {
+                this.dialogService.openAlert({
+                    message: 'There was an error saving your changes, please try again.',
+                });
+            });
     }
 
 }
